fce4936a0daf3f64d5c0758499e8dbaed1716ead
JacekEBA <jacek@ebautomations.com>
2025-10-02 22:52:50 -0400

    Update tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: "hsl(var(--card))",
+        accent: "hsl(var(--accent))",
+        muted: "hsl(var(--muted))",
+      },
+    },
+  },
+  plugins: [],
+};
